09613d9f663ed59e91893ebc0da2d95f83fe0be9
Lynn0108 <liaorui94@163.com>
2017-02-27 18:46:36 +0800

    Delete regexrm.js

--- regexrm.js
+++
@@ -1,8 +0,0 @@
-function numberTest(text) {
-	var regText = /1[358][0-9]{9}/;
-	return text.test(regText);
-}
-function copyTest(text) {
-	var regText = /\b(\w+)\b\s+\1\b/;
-	return text.test(regText);
-}
