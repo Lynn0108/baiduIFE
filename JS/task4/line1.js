b9f492b149f8f24f9b3d42ac2d54d7ec226ce74b
Lynn <landiao_lr@qq.com>
2017-03-16 20:45:32 +0800

    任务4优化

--- JS/task4/line1.js
+++ JS/task4/line1.js
@@ -3,47 +3,45 @@ window.onload = function () {
 	if (!document.getElementById || !document.getElementById('arr') || !document.getElementById('num')) return false;
 
 	var btns = document.getElementsByTagName('button');
+	var input = document.getElementById('num');
+	var ArrP = document.getElementById('arr');
 	var arr = new Array();
 	//清空输入框
-	document.getElementById('num').value = "";
+	input.value = "";
+	//创建节点
+	function createSpnode(text) {
+		window.numNode = document.createElement("span");
+		//给新增节点绑定事件
+		numNode.onclick = function(){
+            ArrP.removeChild(this);
+        }
+        numNode.innerHTML = text;
+	}
+
 	//---给按钮绑定事件---
 	//leftIn
 	btns[0].onclick = function () {
-		var num = document.getElementById('num').value;
-		//var idname = this.id;
+		var num = input.value;
 		//判断输入是否为数字
 		if (isNaN(num) || (num == "")) {
 			alert('请输入数字');
 			return false;
 		}
 		arr.unshift(num);
-		var numNode = document.createElement("span");
-		numNode.onclick = function(){
-                document.getElementById('arr').removeChild(this);
-        }
-		if (arr.length == 1) {
-			document.getElementById('arr').appendChild(numNode);
-		} else {
-			var firstNode = document.getElementById('arr').firstChild;
-			document.getElementById('arr').insertBefore(numNode,firstNode);
-		}
-		numNode.innerHTML = num;
+		createSpnode(num);
+		ArrP.insertBefore(numNode,ArrP.childNodes[0]);
 	}
 	//rightIn
 	btns[1].onclick = function () {
-		var num = document.getElementById('num').value;
+		var num = input.value;
 		//判断输入是否为数字
 		if (isNaN(num) || (num == "")) {
 			alert('请输入数字');
 			return false;
 		}
 		arr.push(num);
-		var numNode = document.createElement("span");
-		numNode.onclick = function(){
-                document.getElementById('arr').removeChild(this);
-        }
-		document.getElementById('arr').appendChild(numNode);
-		numNode.innerHTML = num;
+		createSpnode(num);
+		ArrP.appendChild(numNode);
 	}
 	//leftOut
 	btns[2].onclick = function () {
@@ -52,8 +50,8 @@ window.onload = function () {
 			return false;
 		}
 		arr.shift(arr[arr.length - 1]);
-		var firstNode = document.getElementById('arr').firstChild;
-		document.getElementById('arr').removeChild(firstNode);
+		var firstNode = ArrP.firstChild;
+		ArrP.removeChild(firstNode);
 	}
 	btns[3].onclick = function () {
 		if (arr.length == 0) {
@@ -61,7 +59,7 @@ window.onload = function () {
 			return false;
 		}
 		arr.pop(arr[arr.length - 1]);
-		var lastNode = document.getElementById('arr').lastChild;
-		document.getElementById('arr').removeChild(lastNode);
+		var lastNode = ArrP.lastChild;
+		ArrP.removeChild(lastNode);
 	}
 }
